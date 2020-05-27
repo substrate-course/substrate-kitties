47f171f0aab6a59bc1a8dd1e33be68c01b4a0c6e
Bryan Chen <xlchen1291@gmail.com>
2020-05-27 22:23:32 +1200

    store Kitty ownership information

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -17,6 +17,11 @@ decl_storage! {
 		pub Kitties get(fn kitties): map hasher(blake2_128_concat) u32 => Kitty;
 		/// Stores the total number of kitties. i.e. the next kitty index
 		pub KittiesCount get(fn kitties_count): u32;
+
+		/// Get kitty ID by account ID and user kitty index
+		pub OwnedKitties get(fn owned_kitties): map hasher(blake2_128_concat) (T::AccountId, u32) => u32;
+		/// Get number of kitties by account ID
+		pub OwnedKittiesCount get(fn owned_kitties_count): map hasher(blake2_128_concat) T::AccountId => u32;
 	}
 }
 
@@ -34,19 +39,28 @@ decl_module! {
 		#[weight = 0]
 		pub fn create(origin) {
 			let sender = ensure_signed(origin)?;
-			let count = Self::kitties_count();
-			if count == u32::max_value() {
+			let kitty_id = Self::kitties_count();
+			if kitty_id == u32::max_value() {
 				return Err(Error::<T>::KittiesCountOverflow.into());
 			}
+
+			// Generate a random 128bit value
 			let payload = (
 				<pallet_randomness_collective_flip::Module<T> as Randomness<T::Hash>>::random_seed(),
-				sender,
+				&sender,
 				<frame_system::Module<T>>::extrinsic_index(),
 			);
 			let dna = payload.using_encoded(blake2_128);
+
+			// Create and store kitty
 			let kitty = Kitty(dna);
-			Kitties::insert(count, kitty);
-			KittiesCount::put(count + 1);
+			Kitties::insert(kitty_id, kitty);
+			KittiesCount::put(kitty_id + 1);
+
+			// Store the ownership information
+			let user_kitties_id = Self::owned_kitties_count(&sender);
+			<OwnedKitties<T>>::insert((sender.clone(), user_kitties_id), kitty_id);
+			<OwnedKittiesCount<T>>::insert(sender, user_kitties_id + 1);
 		}
 	}
 }
